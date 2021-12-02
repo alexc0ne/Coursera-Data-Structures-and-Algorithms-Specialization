a78295df8e38da9210046d253c7dbd5e0821a0ed
alexc0ne <alexnederlands@yandex.ru>
2021-12-02 06:23:02 +0100

    new versions of 4th and 5th tasks

--- Course 1 - Algorithmic Toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences.cpp
+++ Course 1 - Algorithmic Toolbox/week5_dynamic_programming1/4_longest_common_subsequence_of_two_sequences.cpp
@@ -6,21 +6,6 @@ using vI = vector<Int>;
 using Matrix = vector<vI>;
 
 
-/*
- * Attention!
- * Much easier to solve (even without filling the base) if we create matrix of size m+1, n+1 like this:
- *
- * . x 3 4 8 2
- * x 0 0 0 0 0
- * 4 0 . <-- starting point
- * 3 0
- * 1 0
- * 2 0
- * 
- */
-
-
-
 vI ReadVector()
 {
     Int n;
@@ -36,45 +21,34 @@ vI ReadVector()
 Int CommonSubsequenceOfTwo(const vI & first, const vI & second)
 {
     if (first.empty() or second.empty()) return 0;
+    Matrix m (first.size() + 1, vI (second.size() + 1, 0));
 
-    Matrix m (first.size(), vI (second.size(), 0));
-
-
-	// base ========================================    
+    // base (automatically) ========================
     /*
      *
-     *   A C B B C A
-     * B 0 0 1 1 1 1
-     * A 1
-     * C 1
+     *   0 A C B B C A
+     * 0 0 0 0 0 0 0 0
+     * B 0
+     * A 0
+     * C 0
      *
-     * id est (B, A) = 0
-     *        (B, AC) = 0
-     *        (B, ACB) = 1
-     *        (B, ACBB) = 1 and so on.
      *
-     * */    
-    
-    if (first[0] == second[0]) m[0][0] = 1;
-    
-    for (size_t i = 1; i < first.size(); ++i)
-        if (m[i-1][0] or first[i] == second[0]) m[i][0] = 1;
-
-    for (size_t j = 1; j < second.size(); ++j)
-        if (m[0][j-1] or second[j] == first[0]) m[0][j] = 1;
-
+     * */
 
     // dynamic =====================================
-    for (size_t i = 1; i < first.size(); ++i)
-        for (size_t j = 1; j < second.size(); ++j)
+    for (size_t i = 1; i <= first.size(); ++i)
+        for (size_t j = 1; j <= second.size(); ++j)
         {
+            // ('abC', 'acC') --> 1 + max(('ab'), ('ac'))
+            if(first[i-1] == second[j-1])
+            {
+                m[i][j] = m[i-1][j-1] + 1;
+                continue;
+            }
+
             // ('bacd', 'caab') --> max of 2 subtasks:
             // ('bacd', 'caa') and ('bac', 'caab')
             m[i][j] = max(m[i-1][j], m[i][j-1]);
-
-            // ('abC', 'acC') --> 1 + max(('ab'), ('ac'))
-            if (first[i] == second[j])
-                m[i][j] = max(m[i][j], 1 + m[i-1][j-1]);
         }
 
     return m.back().back();
